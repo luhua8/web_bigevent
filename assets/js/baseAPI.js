219c6bd3c226e6deb6fb38209484d73b2b4a3317
luhua8 <2080408744@qq.com>
2021-06-06 10:40:36 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,23 @@
 $.ajaxPrefilter(function(options) {
     // 在发起真正的ajax请求之前,统一拼接请求的根路径.
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+
+    // 统一为有权限的接口设置headers请求头
+    // 如果url中字符/my/的索引不等于-1说明url中含有字符/my/
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function(res) {
+        // 再complete回调函数中可以使用res.responseJSO拿到服务器相应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token');
+            // 2.强制跳转到登录页面
+            location.href = '/login.html';
+        }
+    }
 })
